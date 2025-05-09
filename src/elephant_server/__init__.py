b7f96ec2d65f84abe8b600c4d03da7304892f6dc
Sebastian Spreizer <spreizer@web.de>
2025-05-09 17:58:58 +0200

    Fix __init__

--- src/elephant_server/__init__.py
+++ src/elephant_server/__init__.py
@@ -2,6 +2,7 @@ from importlib import metadata # noqa
 
 try:
     __version__ = metadata.version("elephant-server")
-    del metadata
-except:
+except metadata.PackageNotFoundError:
     pass
+
+del metadata
